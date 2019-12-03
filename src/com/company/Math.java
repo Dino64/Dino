80d92bb9c0f20776ea8a7235577eab566cfdf3e7
Dino64 <dino.dsu@gmail.com>
2019-12-03 09:57:18 +0100

    Added metohod to the math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,5 +1,22 @@
 package com.company;
 
 public class Math {
-    
+    public static void main(String[] args) {
+        Math myApp = new Math();
+        int a = 5;
+        int b = 10;
+int sum;
+        sum = myApp.add(a,b);
+        System.out.println(sum);
+
+
+
+
+    }
+
+    public int add (int first, int second){
+        int sum = 0;
+        sum = first + second;
+        return sum;
+    }
 }
